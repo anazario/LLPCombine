016a96c1f987686aad961e945e0efe4a75079574
Andrés J Abreu <andres.abreu@ku.edu>
2025-10-18 15:05:48 -0500

    Debugging

--- include/ConfigParser.h
+++ include/ConfigParser.h
@@ -97,7 +97,6 @@ struct AnalysisConfig {
     std::vector<std::string> backgrounds;
     std::vector<std::string> signals;
     std::vector<std::string> signal_points;  // Optional: specific signal points to use
-    std::vector<std::string> data;  // Data samples from config
     
     std::vector<BinConfig> bins;
     std::vector<SystematicConfig> systematics;
